83d73766b0fb0a98c9ffca170d98c3d825a40e19
Omar Besiso <omarbesiso@outlook.com>
2017-05-31 05:26:57 +1000

    Guid Factory tests

---
+++ src/Ximo.Tests/Utilities/GuidFactoryTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ximo.Utilities;
+
+namespace Ximo.Tests.Utilities
+{
+    /// <summary>
+    ///     Test for testing <see cref="GuidFactory" />
+    /// </summary>
+    [TestClass]
+    public class GuidFactoryTests
+    {
+        /// <summary>
+        ///     Tests if generated guids are sequential.
+        /// </summary>
+        [TestMethod]
+        public void TestIfGeneratedGuidsAreSequential()
+        {
+            var arraySize = 1000;
+            var array = new Guid[arraySize];
+            array[0] = GuidFactory.NewGuidComb();
+
+            for (var i = 1; i < arraySize; i++)
+            {
+                array[i] = GuidFactory.NewGuidComb();
+                Assert.AreEqual(-1, array[i - 1].CompareTo(array[i]));
+            }
+        }
+    }
+}
--- src/Ximo/Utilities/GuidFactory.cs
+++ src/Ximo/Utilities/GuidFactory.cs
@@ -7,6 +7,8 @@ namespace Ximo.Utilities
     /// </summary>
     public static class GuidFactory
     {
+        private static readonly long BaseDateTicks = new DateTime(1900, 1, 1).Ticks;
+
         /// <summary>
         ///     Generate a new <see cref="Guid" /> using the comb algorithm.
         /// </summary>
@@ -19,29 +21,26 @@ namespace Ximo.Utilities
         /// <returns>A new <see cref="Guid" />.</returns>
         public static Guid NewGuidComb()
         {
-            var guidArray = Guid.NewGuid().ToByteArray();
+            byte[] guidArray = Guid.NewGuid().ToByteArray();
 
-            var baseDate = new DateTime(1900, 1, 1);
-            var now = DateTime.Now;
+            DateTime now = DateTime.UtcNow;
 
             // Get the days and milliseconds which will be used to build the byte string 
-            var ticks = now.Ticks - baseDate.Ticks;
-            var days = new TimeSpan(ticks);
-            var milliseconds = now.TimeOfDay;
+            TimeSpan days = new TimeSpan(now.Ticks - BaseDateTicks);
+            TimeSpan msecs = now.TimeOfDay;
 
             // Convert to a byte array 
             // Note that SQL Server is accurate to 1/300th of a millisecond so we divide by 3.333333 
-            var daysArray = BitConverter.GetBytes(days.Days);
-            var accuracyValue = (long) (milliseconds.TotalMilliseconds / 3.333333);
-            var millisecondsArray = BitConverter.GetBytes(accuracyValue);
+            byte[] daysArray = BitConverter.GetBytes(days.Days);
+            byte[] msecsArray = BitConverter.GetBytes((long)(msecs.TotalMilliseconds / 3.333333));
 
             // Reverse the bytes to match SQL Servers ordering 
             Array.Reverse(daysArray);
-            Array.Reverse(millisecondsArray);
+            Array.Reverse(msecsArray);
 
             // Copy the bytes into the guid 
             Array.Copy(daysArray, daysArray.Length - 2, guidArray, guidArray.Length - 6, 2);
-            Array.Copy(millisecondsArray, millisecondsArray.Length - 4, guidArray, guidArray.Length - 4, 4);
+            Array.Copy(msecsArray, msecsArray.Length - 4, guidArray, guidArray.Length - 4, 4);
 
             return new Guid(guidArray);
         }
